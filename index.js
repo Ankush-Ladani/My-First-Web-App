546446316aceb4a2d53536fc0faf1020da43dd89
Ladani Ankush <astarladani@gmail.com>
2022-01-29 08:48:03 +0530

    docs : index file of html deleted and js file non-commented

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
-// console.log("---------------")
-// console.log("Ensure that You Use First Letter Capital Of Every Input")
-// console.log("---------------")
-// var readlineSync = require("readline-sync");
+console.log("---------------")
+console.log("Ensure that You Use First Letter Capital Of Every Input")
+console.log("---------------")
+var readlineSync = require("readline-sync");
 
-// var UserName = readlineSync.question("What's Your Name? ");
+var UserName = readlineSync.question("What's Your Name? ");
 
-// console.log("Welcome " + UserName + " to Do You Know Me Game? ");
+console.log("Welcome " + UserName + " to Do You Know Me Game? ");
 
-// var score = 0;
+var score = 0;
 
 
-// function play(question,answer){
-//   var userAnswer = readlineSync.question(question);
+function play(question,answer){
+  var userAnswer = readlineSync.question(question);
 
-//   if(userAnswer.toLowerCase() === answer.toLowerCase()){
-//     console.log("Right!")
-//     score++;
-//   }
-//   else{
-//     console.log("Wrong!")
-//   }
+  if(userAnswer.toLowerCase() === answer.toLowerCase()){
+    console.log("Right!")
+    score++;
+  }
+  else{
+    console.log("Wrong!")
+  }
 
-//   console.log("Current Score is : " + score)
-//   console.log("-------------------")
-// }
+  console.log("Current Score is : " + score)
+  console.log("-------------------")
+}
 
-// var questionOne = {
-//   question : "Where do I Live? ",
-//   answer1 : "Jamjodhpur"
-// }
-// var questionTwo = {
-//   question : "Which Sports I Like To Play? ",
-//   answer1 : "Cricket"
-// }
+var questionOne = {
+  question : "Where do I Live? ",
+  answer1 : "Jamjodhpur"
+}
+var questionTwo = {
+  question : "Which Sports I Like To Play? ",
+  answer1 : "Cricket"
+}
 
-// var questions = [questionOne,questionTwo];
+var questions = [questionOne,questionTwo];
 
-// for(var i=0;i<questions.length;i++){
-//     play(questions[i].question,questions[i].answer1);
-// }
-// console.log("Your Final Score is " + score);
+for(var i=0;i<questions.length;i++){
+    play(questions[i].question,questions[i].answer1);
+}
+console.log("Your Final Score is " + score);
 
